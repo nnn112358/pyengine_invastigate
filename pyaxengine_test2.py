a551d59bf97cf9da1bfe456e22d46476fca44947
nnn <27625496+nnn112358@users.noreply.github.com>
2025-01-22 12:49:56 +0900

    Add files via upload

---
+++ pyaxengine_test2.py
@@ -0,0 +1,18 @@
+import axengine as axe
+import numpy as np
+
+def main():
+    try:
+        input_data = np.random.randint(0, 255, (1, 640, 640, 3), dtype=np.uint8)
+        session = axe.InferenceSession('yolo11s.axmodel')
+        outputs = session.run([o.name for o in session.get_outputs()], 
+                           {session.get_inputs()[0].name: input_data})
+        
+        np.save('outputs.npy', outputs[0])
+        print("Outputs saved to outputs.npy")
+        
+    except Exception as e:
+        print(f"error: {e}")
+
+if __name__ == "__main__":
+    main()
